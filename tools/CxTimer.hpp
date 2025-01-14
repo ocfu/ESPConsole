95fe8bf644ed14c7d00d23952b3f43f925e4f4b9
ocfu <keolafu@arcor.de>
2025-01-14 22:09:38 +0100

    amend CxTimer behavior and add getter methods for elapsed time, period, hold and running state.

--- tools/CxTimer.hpp
+++ tools/CxTimer.hpp
@@ -10,29 +10,38 @@
 #define CxTimer_hpp
 
 class CxTimer {
-   uint32_t _interval;
+   uint32_t _nPeriod;
    uint32_t _last;
    bool _bOnHold;
    bool _bFinish;  // if true, trigger the next isDue() call
    
 public:
-   CxTimer(uint32_t interval) : _interval(interval), _last(0), _bOnHold((interval == 0)), _bFinish(false) {start();}
+   CxTimer(uint32_t period, bool hold = false) : _nPeriod(period), _last(0), _bOnHold(hold||(period == 0)), _bFinish(false) {if (!hold) start();}
    CxTimer() : CxTimer(0) {}
    
-   void start() {_last = (uint32_t)millis(); _bOnHold = false;}
+   void start() {_last = (uint32_t)millis(); _bOnHold = (_nPeriod == 0);}
    void restart() {start();}
-   void start(uint32_t interval) {_interval = interval; start();}
+   void start(uint32_t period) {_nPeriod = period; start();}
+   void startOnChange(uint32_t period) {if (_nPeriod != period) start(period);}
    void finish() {_bFinish = true;}
+
+   uint32_t getElapsedTime() {return ((uint32_t)millis() - _last);}
+   uint32_t getPeriod() {return _nPeriod;}
    
    bool isDue(bool hold = false) {
-      if (!_bOnHold && (_bFinish || (millis() - _last) > _interval)) {
-         start();
+      if (!_bOnHold && (_bFinish || (millis() - _last) > _nPeriod)) {
+         if (!hold) restart();
          _bOnHold = hold;
          _bFinish = false;
          return true;
       }
       return false;
    }
+   
+   bool isOnHold() {return _bOnHold;}
+   
+   bool isRunning() {return (!isOnHold() && _nPeriod);}
+   
 };
 
 class CxTimer1s : public CxTimer {
