a85ca30b0d567a9b8f194a4ba93785838bc6f829
ocfu <keolafu@arcor.de>
2025-01-19 00:03:58 +0100

    refactor members and add stop() and setPeriod() methods. amend start() methods.

--- tools/CxTimer.hpp
+++ tools/CxTimer.hpp
@@ -13,26 +13,29 @@ class CxTimer {
    uint32_t _nPeriod;
    uint32_t _last;
    bool _bOnHold;
-   bool _bFinish;  // if true, trigger the next isDue() call
+   bool _isDue;  // if true, trigger the next isDue() call
    
 public:
-   CxTimer(uint32_t period, bool hold = false) : _nPeriod(period), _last(0), _bOnHold(hold||(period == 0)), _bFinish(false) {if (!hold) start();}
+   CxTimer(uint32_t period, bool hold = false) : _nPeriod(period), _last(0), _bOnHold(hold||(period == 0)), _isDue(false) {if (!hold) start();}
    CxTimer() : CxTimer(0) {}
    
    void start() {_last = (uint32_t)millis(); _bOnHold = (_nPeriod == 0);}
+   void stop() {_bOnHold = true;}
    void restart() {start();}
-   void start(uint32_t period) {_nPeriod = period; start();}
+   void start(uint32_t period, bool bMakeDue = false) {_nPeriod = period; start(); if (bMakeDue) makeDue();}
+   void start(bool bMakeDue) {start(); if (bMakeDue) makeDue();}
    void startOnChange(uint32_t period) {if (_nPeriod != period) start(period);}
-   void finish() {_bFinish = true;}
+   void makeDue() {_isDue = true;}
 
    uint32_t getElapsedTime() {return ((uint32_t)millis() - _last);}
    uint32_t getPeriod() {return _nPeriod;}
+   void setPeriod(uint32_t set) {_nPeriod = set;}
    
    bool isDue(bool hold = false) {
-      if (!_bOnHold && (_bFinish || (millis() - _last) > _nPeriod)) {
+      if (!_bOnHold && (_isDue || (millis() - _last) > _nPeriod)) {
          if (!hold) restart();
          _bOnHold = hold;
-         _bFinish = false;
+         _isDue = false;
          return true;
       }
       return false;
@@ -46,17 +49,17 @@ public:
 
 class CxTimer1s : public CxTimer {
 public:
-   CxTimer1s() : CxTimer(1000) {}
+   CxTimer1s(bool bHold = false) : CxTimer(1000, bHold) {}
 };
 
 class CxTimer10s : public CxTimer {
 public:
-   CxTimer10s() : CxTimer(10000) {}
+   CxTimer10s(bool bHold = false) : CxTimer(10000, bHold) {}
 };
 
 class CxTimer60s : public CxTimer {
 public:
-   CxTimer60s() : CxTimer(60000) {}
+   CxTimer60s(bool bHold = false) : CxTimer(60000, bHold) {}
 };
 
 #endif /* CxTimer_hpp */
