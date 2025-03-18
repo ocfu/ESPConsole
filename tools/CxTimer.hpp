6ce2139765120d04728fbbc05bba67e2c01cdf54
ocfu <keolafu@arcor.de>
2025-03-18 08:19:45 +0100

    improve makeDue() method. adds documenation.

--- tools/CxTimer.hpp
+++ tools/CxTimer.hpp
@@ -1,19 +1,27 @@
-//
-//  CxTimer.hpp
-//  xESP
-//
-//  Created by ocfu on 25.09.24.
-//  Copyright © 2024 ocfu. All rights reserved.
-//
-
+/**
+ * @file CxTimer.hpp
+ * @brief Timer class for ESP-based projects
+ * @details This file defines the `CxTimer` class, which provides a timer capability for ESP-based projects.
+ * The class includes methods for setting up, managing, and executing timers.
+ *
+ * @date created by ocfu on 13.12.24.
+ * @copyright © 2024 ocfu
+ *
+ */
 #ifndef CxTimer_hpp
 #define CxTimer_hpp
 
+/**
+ * @class CxTimer
+ * @brief Timer class for managing time intervals.
+ * @details The `CxTimer` class provides a timer capability for ESP-based projects.
+ * It includes methods for setting up, managing, and executing timers.
+ */
 class CxTimer {
-   uint32_t _nPeriod;
-   uint32_t _last;
-   bool _bOnHold;
-   bool _isDue;  // if true, trigger the next isDue() call
+   uint32_t _nPeriod; ///< The timer period in milliseconds
+   uint32_t _last; ///< The time of the last timer start
+   bool _bOnHold; ///< Flag to indicate if the timer is on hold
+   bool _isDue;  ///< if true, trigger the next isDue() call
    
 public:
    CxTimer(uint32_t period, bool hold = false) : _nPeriod(period), _last(0), _bOnHold(hold||(period == 0)), _isDue(false) {if (!hold) start();}
@@ -25,12 +33,13 @@ public:
    void start(uint32_t period, bool bMakeDue = false) {_nPeriod = period; start(); if (bMakeDue) makeDue();}
    void start(bool bMakeDue) {start(); if (bMakeDue) makeDue();}
    void startOnChange(uint32_t period) {if (_nPeriod != period) start(period);}
-   void makeDue() {_isDue = true;}
+   void makeDue() {_isDue = (_nPeriod > 0);}
 
    uint32_t getElapsedTime() {return ((uint32_t)millis() - _last);}
    uint32_t getPeriod() {return _nPeriod;}
    void setPeriod(uint32_t set) {_nPeriod = set;}
    
+   /** Check if the timer is due and restart it if not on hold */
    bool isDue(bool hold = false) {
       if (!_bOnHold && (_isDue || (millis() - _last) > _nPeriod)) {
          if (!hold) restart();
